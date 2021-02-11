7f23c0421d08dc340283cf9d12638b75d91f5d95
elomedah <elomedah@gmail.com>
2021-02-11 10:59:02 +0100

    Exemple map reduce

---
+++ MapReduce-Python-master/Exemple-1/python-3/mapper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys
+wordList = dict()
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split('|')
+    print('{},{}'.format(words[2], words[3]))
